715ee42bb55186542c5687e2fb16749ccabd23e6
Md Alif Babu <alifcse49@gmail.com>
2024-07-31 19:14:57 +0600

    java io repo

---
+++ UserInput/src/InputScanner.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class InputScanner {
+
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+		
+		int a = scanner.nextInt();
+		double b = scanner.nextDouble();
+		
+		System.out.println(a*b);
+	}
+
+}
+    
